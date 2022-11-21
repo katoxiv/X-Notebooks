22da59264fce48ab5f76bcbdb915466f9bc38e7d
sourasen1011 <souradeepsen2711@gmail.com>
2022-11-21 16:54:36 +0000

    removed to_coord function from 03_Mapping. put it in utils

--- Twitter_Coursework/utils.py
+++ Twitter_Coursework/utils.py
@@ -35,3 +35,32 @@ from pprint import pprint
 # Define static variables
 in_folder = "Twitter Data"
 out_file = "all_june_tweets.txt"
+
+# Some Functions
+def to_coord(df_original , coord_cols):
+    '''
+    takes coordinate column [x.xxx , y.yyy] and returns df with latitude and longitude separated
+    params : df_original -> dataframe on which conversion is to be carried out
+    params : columns with coordinates
+    returns: df with converted columns
+    '''
+    # Make copy
+    df = df_original.copy()
+
+    for coord_col in coord_cols:
+        long_col = coord_col + '_long'
+        lat_col = coord_col + '_lat'
+        # Long and Lat - clean up
+        df[[long_col , lat_col]] = df[coord_col].str.split(',' , expand = True)
+        # Long
+        df[long_col] = df[long_col].apply(lambda x : x[1:])#.astype('float')
+        df[long_col] = df[long_col].astype('float')
+        # Lat
+        df[lat_col] = df[lat_col].apply(lambda x : x[:-1])#.astype('float')
+        df[lat_col] = df[lat_col].astype('float')
+
+        # remove coord col
+        df.drop(coord_col , axis = 1 , inplace = True)
+    
+    # return modified df
+    return df
