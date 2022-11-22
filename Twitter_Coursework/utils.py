46bef309e5560b2c4102cb73db575287b88f23a0
sourasen1011 <souradeepsen2711@gmail.com>
2022-11-22 21:38:49 +0000

    bunch of mapping utils

--- Twitter_Coursework/utils.py
+++ Twitter_Coursework/utils.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 from multiprocessing import Pool
 import time
-# from concurrent import futures
+
 import csv
 import pickle
 import ast
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon as PolygonPatch
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import LinearSegmentedColormap
-import mplcursors as mpc
+from bng_latlon import OSGB36toWGS84
 
 import matplotlib
 import seaborn as sns
@@ -64,3 +64,65 @@ def to_coord(df_original , coord_cols):
     
     # return modified df
     return df
+
+def layout(shape_file):
+    # Euro Road Layout - 2min31.7sec - https://osdatahub.os.uk/downloads/open/OpenRoads
+    '''
+    Helper function to read and store info from .shp files
+    params: shape_file: path to a specific shape_file
+    returns: patches: list with shapely objects from shape_file
+    '''
+    patches = []
+
+    with fiona.open(shape_file) as layers:
+        for feature in layers:
+            p = shape(feature['geometry'])
+            try:
+                # idk..something weird is happening with the lats and longs - some sort of switch. swapped them for now
+                lons , lats = np.array(p.xy)
+                lons_and_lats = list(zip(lons, lats))
+                wgs84_coord = []
+                for lon_and_lat in lons_and_lats:
+                    wgs84_coord.append(OSGB36toWGS84(lon_and_lat[0] , lon_and_lat[1])[::-1])
+                patches.append(PolygonPatch(wgs84_coord , closed = False))
+            except AttributeError:
+                for poly in p.geoms:
+                    # idk..something weird is happening with the lats and longs - some sort of switch. swapped them for now
+                    lons , lats = np.array(poly.xy)
+                    lons_and_lats = list(zip(lons, lats))
+                    wgs84_coord = []
+                    for lon_and_lat in lons_and_lats:
+                        wgs84_coord.append(OSGB36toWGS84(lon_and_lat[0] , lon_and_lat[1])[::-1])
+                    patches.append(PolygonPatch(wgs84_coord , closed = False))
+    
+    return patches
+
+def get_patches(path_to_shape_files):
+    '''
+    params: apply_layout: layout func
+    params: path_to_shape_files: path to various shape files that need to be plotted
+
+    returns: patches: list with all shape file geometries attached
+    '''
+    patches = [] # storage container
+    with Pool() as pool_exec:
+        results = pool_exec.map(layout  , path_to_shape_files)
+        # Iterating through Generator Object
+        for result in results:
+            patches.append(result)
+    return patches
+
+def test_func(x):
+    return x**2
+
+def test_pool(test_func , L):
+    '''
+    just a func to test if pool is working
+    '''
+    l_0 = []
+    with Pool() as pool_exec:
+        results = pool_exec.map(test_func  , L)
+        # Iterating through Generator Object
+        for result in results:
+            l_0.append(result)
+    return l_0
